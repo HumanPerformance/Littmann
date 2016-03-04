717e8c6ef0341907f89ededc8c5bb9348ef84cba
Fluvio Lobo <fluvio.lobo@gmail.com>
2016-03-04 15:50:34 -0500

    Had created but soon eliminated the "Control" function which was intended to use processing and controlP5 libraries. Eclipse has its own GUI builder so it should be more straightforward to use such mechanism.

---
+++ Eclipse/src/Control.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author flobo
+ *
+ */
+public class Control {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
